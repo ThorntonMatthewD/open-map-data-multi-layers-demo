4c821cb27a72a8e907c8f468ee589614260086bc
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-10-04 11:36:08 -0400

    Fix type

--- override-fetch.ts
+++ override-fetch.ts
@@ -2,15 +2,24 @@ import { afterAll, beforeAll } from "vitest";
 
 const original_fetch = global.fetch;
 
-// Using any here for now
-const get_proxied_url = (url: any) => {
-  const newURL = typeof url === "object" ? url : new URL(url);
+const get_proxied_url = (url: RequestInfo | URL) => {
+  let href = "";
+
+  if (typeof url === "string") {
+    href = url;
+  } else {
+    href = "href" in url ? url.href : get_link_from_request_info(url);
+  }
+
+  const newURL = new URL(href);
   newURL.host = "localhost";
   newURL.port = "9090";
   newURL.protocol = "http:";
 
   return newURL.href;
 };
+
+const get_link_from_request_info = (reqInfo: Request) => reqInfo.url;
 
 beforeAll(() => {
   global.fetch = async (url) => {

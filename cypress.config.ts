dfa418b300417194ba32663425843187659450b5
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-10-04 22:05:26 -0400

    Move cypress folders

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
+    specPattern: "src/tests/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173/open-map-data-multi-layers-demo/",
+    fixturesFolder: "src/tests/cypress/fixtures",
+    screenshotsFolder: "src/tests/cypress/screenshots",
+    videosFolder: "src/tests/cypress/videos",
+    downloadsFolder: "src/tests/cypress/downloads",
+    supportFile: "src/tests/cypress/support/e2e.ts",
   },
 });

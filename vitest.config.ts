91197745b6dc268dd21e0447b78d494e3cac0f0f
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-10-04 23:28:30 -0400

    Remove dotenv

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globalSetup: ["./src/tests/vitest.global.ts"],
-    setupFiles: ["dotenv/config"],
   },
 });
